16d07c381eaae2dbe3c67f44e508a0572849ecbc
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-24 11:43:22 +0300

    server basics
    1 Get endpoint
    2 Post endpoints
    
    according to requirements

--- server/app.js
+++ server/app.js
@@ -1,5 +1,46 @@
 const express = require('express');
+const fs = require("fs").promises;
 const app = express();
 
+const path = process.env.MY_VARIABLE||"./data.json"
 
+
+app.get("/api/tickets/:searchText", async (req, res) => {
+    const content = await fs.readFile(path);
+    const tickets = JSON.parse(content);
+    // const lowerCaseSearchText = searchText.toLowerCase
+    const lowerCaseSearchText = req.params.id.toLowerCase()
+    filteredTickets = tickets.filter(ticket => ticket["title"].toLowerCase().includes(lowerCaseSearchText))
+    res.send(filteredTickets);
+})
+
+
+app.post("api/tickets/:ticketId/done", (req, res => {
+    const content = await fs.readFile(path);
+    const tickets = JSON.parse(content);
+    tickets.forEach(ticket, i => {
+        if(ticket["id"] = req.params.ticketId) {
+            ticket["done"] = true
+        }
+    await fs.writeFile(path, JSON.stringify(ticket));
+    res.send(tickets);
+    });
+}))
+
+
+app.post("api/tickets/:ticketId/undone", (req, res => {
+    const content = await fs.readFile(path);
+    const tickets = JSON.parse(content);
+    tickets.forEach(ticket, i => {
+        if(ticket["id"] = req.params.ticketId) {
+            ticket["done"] = false
+        }
+    await fs.writeFile(path, JSON.stringify(ticket));
+    res.send(tickets);
+    });
+}))
+
+
+
+app.listen(3000, () => console.log("listening on port 4000..."));
 module.exports = app;
