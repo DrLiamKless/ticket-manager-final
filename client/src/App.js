35bda4cef466cb968d8fd6c51e13331db465c7ff
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-24 19:22:01 +0300

    Revert "passing 4 first tests"
    This reverts commit 8239390580324d47151f3f14028845d7a8dbdb21.

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
-import './App.css';
 import Ticket from './components/Ticket'
-import Search from './components/Search';
-import HiddenCounter from './components/HiddenCounter';
+import './App.css';
 
 function App() {
 
   const [tickets, setTickets] = useState([]);
-  const [searchText, setSearchText] = useState([])
-  const [partialList, setPartialList] = useState([])
-  const [numberOfHidden, setNumberOfHidden] = useState(0)
 
   useEffect(() => {
-    axios.get(`/api/tickets?searchText=${searchText}`).then(res => {
+    axios.get("/api/tickets?:searchText=").then(res => {
       setTickets(res.data);
     });
-    }, [searchText]
+    }, []
   )
 
 
 
   return (
     <main>
-      <HiddenCounter
-      numberOfHidden={numberOfHidden}></HiddenCounter>
-      <Search
-        searchText={searchText}
-        setSearchText={setSearchText}>  
-      </Search>
-
       {tickets.map((ticket, i) => (
           <Ticket
             key={i}
             title={ticket["title"]}
             content={ticket["content"]}
             userEmail={ticket["userEmail"]}
-            creationTime={ticket["creationTime"]}
-            labels={ticket["labels"]}
-            partialList={partialList}
-            setPartialList={setPartialList}
-            tickets={tickets}
-            setTickets={setTickets}
-            numberOfHidden={numberOfHidden}
-            setNumberOfHidden={setNumberOfHidden}>
+            creationTime={ticket["creationTime"]}>
           </Ticket>
         ))}
     </main>
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,28 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import HideButton from './HideButton';
 
 function Ticket(props) {
 
     return (
         <div className={"ticket"}>
-            <HideButton
-            ticketIndex={props.key}
-            partialList={props.partialList}
-            setPartialList={props.setPartialList}
-            tickets={props.tickets}
-            setTickets={props.setTickets}
-            numberOfHidden={props.numberOfHidden}
-            setNumberOfHidden={props.setNumberOfHidden}>
-            </HideButton>
-
-        
             <h1>{props.title}</h1>
             <p>{props.content}</p>
             <p>{props.userEmail} | {props.creationTime}</p>
-            {props.labels && props.labels.map((label, i) => (
-                <div key={i} className={"label"}>{label}</div>
-            ))}
+
         </div>
     )
 }
