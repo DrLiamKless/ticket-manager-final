c0448f36c970ee723fb1c1531872cdac031dfc80
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-24 19:42:44 +0300

    restoring

---
+++ client/src/components/HideButton.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function HideButton(props) {
+
+    const toHide = () => {
+        let listToSet = props.tickets.slice();
+        listToSet.splice(props.ticketIndex, 1)
+        props.setTickets(listToSet);
+        props.setNumberOfHidden(props.numberOfHidden + 1)
+    }
+
+    return (    
+        <button
+            className={"hideTicketButton"}
+            onClick={toHide}>hide</button>
+    )
+}
+
+export default HideButton
