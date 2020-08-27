c68e344f8e37fb146459349391420c82e3526bb7
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-27 10:41:57 +0300

    A change for github actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.css';
 import Ticket from './components/Ticket';
 import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
   const [toRestore, setToRestore] = useState(false);
